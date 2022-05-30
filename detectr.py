67d303f081e1d7c08bcd64485855034d2400b5b4
Mark <marknzl@outlook.co.nz>
2022-05-30 13:25:34 +1200

    Update driver code

--- detectr.py
+++ detectr.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot
 from image_utils import (
+    compute_threshold,
     read_rgb_image_to_separate_pixel_arrays,
     compute_rgb_to_greyscale,
     contrast_stretch,
@@ -13,12 +14,17 @@ def main():
 
     greyscale_image = compute_rgb_to_greyscale(px_array_r, px_array_g, px_array_b, image_width, image_height)
     contrast_stretched_image = contrast_stretch(greyscale_image, image_width, image_height)
-    std_dev_image = compute_standard_deviation_image_5x5(contrast_stretched_image, image_width, image_height)
+    std_dev_image = contrast_stretch(compute_standard_deviation_image_5x5(contrast_stretched_image, image_width, image_height), image_width, image_height)
+    thresholded_image = compute_threshold(std_dev_image, 147, image_width, image_height)
 
     fig1, axs1 = pyplot.subplots(2, 2)
 
-    axs1[0, 0].set_title('test')
+    axs1[0, 0].set_title('Standard deviation image')
     axs1[0, 0].imshow(std_dev_image, cmap='gray')
+
+    axs1[0, 1].set_title('Thresholded image')
+    axs1[0, 1].imshow(thresholded_image, cmap='gray')
+
     pyplot.show()
 
 if __name__ == '__main__':
