834c423c4552dc55fc0cb26d16ca09cd48b07391
Mark <marknzl@outlook.co.nz>
2022-05-30 13:05:45 +1200

    Add standard deviation image function

--- image_utils.py
+++ image_utils.py
@@ -1,4 +1,5 @@
 from png import Reader
+from math import sqrt
 
 def read_rgb_image_to_separate_pixel_arrays(input_filename) -> tuple:
     image_reader = Reader(filename=input_filename)
@@ -44,3 +45,55 @@ def compute_rgb_to_greyscale(pixel_array_r, pixel_array_g, pixel_array_b, image_
             g = 0.299 * pixel_array_r[row][col] + (0.587 * pixel_array_g[row][col]) + (0.114 * pixel_array_b[row][col])
             greyscale_pixel_array[row][col] = int(round(g))
     return greyscale_pixel_array
+
+def contrast_stretch(pixel_array, image_width, image_height) -> list[list[int]]:
+    ret = create_initialized_greyscale_pixel_array(image_width, image_height)
+
+    f_min = 255
+    f_max = 0
+    for row in range(0, image_height):
+        for col in range(0, image_width):
+            pixel_val = pixel_array[row][col]
+            f_max = max(f_max, pixel_val)
+            f_min = min(f_min, pixel_val)
+
+    g_min = 0
+    g_max = 255
+
+    for row in range(0, image_height):
+        for col in range(0, image_width):
+            pixel_val = pixel_array[row][col]
+            if f_max == f_min:
+                s_out = g_min
+            else:
+                s_out = (pixel_val - f_min) * ((g_max - g_min) / (f_max - f_min)) + g_min
+            if s_out < g_min:
+                ret[row][col] = g_min
+            elif (g_min <= s_out <= g_max):
+                ret[row][col] = round(s_out)
+            else:
+                ret[row][col] = g_max
+
+    return ret
+
+def compute_standard_deviation_image_5x5(pixel_array, image_width, image_height) -> list[list[int]]:
+    new_image = create_initialized_greyscale_pixel_array(image_width, image_height, 0)
+    for y in range(0, image_height):
+        for x in range(0, image_width):
+            curr_total = 0
+            temp = []
+            for dy in [-2, -1, 0, 1, 2]:
+                for dx in [-2, -1, 0, 1, 2]:
+                    curr_total += pixel_array[y + dy][x + dx]
+                    temp.append(pixel_array[y + dy][x + dx])
+                    
+            mean = curr_total / 25
+            deviation_sum = 0
+            
+            for num in temp:
+                deviation_sum += (num - mean) ** 2
+            
+            variance = deviation_sum / 25
+            std_dev = sqrt(variance)
+            new_image[y][x] = std_dev
+    return new_image
