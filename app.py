1fef97b7ed2a036f5dddf1c2f28d7bba218efed3
Mark <marknzl@outlook.co.nz>
2022-06-08 10:53:16 +1200

    Implement webapp logic

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import os
+
+from flask import Flask, render_template, request
+from os import path
+from hashlib import md5
+from webapp_utils import (
+    get_license_plate,
+    read_license_plate_text,
+    filter_text
+)
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.before_request
+def create_static_folder_if_nonexistent():
+    if not path.exists('static'):
+        os.makedirs('static')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,6 +27,25 @@ def index():
     filtered_plate_text = None
     accuracy = None
     error_msg = None
+
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename.split(".")[1] != "png":
+            error_msg = "Only PNGs allowed!"
+        else:
+            md5_hash = md5(file.read()).hexdigest()
+            hashed_filename = f'{md5_hash}.{file.filename.split(".")[1]}'
+            file.seek(0)    # need to reset cursor position as we call file.read()
+
+            if path.exists(f'static/{hashed_filename}'):    # If same image is already uploaded, we don't process it again.
+                extracted_filename = f'{hashed_filename.split(".")[0]}_plate.{hashed_filename.split(".")[1]}'
+            else:
+                file.save(f'static/{hashed_filename}')
+                extracted_filename = get_license_plate(hashed_filename)
+            raw_plate_text, accuracy = read_license_plate_text(extracted_filename)
+            filtered_plate_text = filter_text(raw_plate_text)
+
+            submitted = True
     
     return render_template('index.html',
                            submitted=submitted,
