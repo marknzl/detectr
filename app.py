6c377061d6656c33f6a6db018fbaff4b0ec1b0c7
Mark <marknzl@outlook.co.nz>
2022-06-08 10:44:17 +1200

    Update template and webapp

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    submitted = False
+    hashed_filename = None
+    extracted_filename = None
+    raw_plate_text = None
+    filtered_plate_text = None
+    accuracy = None
+    error_msg = None
+    
+    return render_template('index.html',
+                           submitted=submitted,
+                           unmodified_filename=hashed_filename,
+                           extracted_filename=extracted_filename,
+                           raw_plate_text=raw_plate_text,
+                           filtered_plate_text=filtered_plate_text,
+                           accuracy=accuracy,
+                           error_msg=error_msg)
