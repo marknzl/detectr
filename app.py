a8260b4ea6f27941f14a68076b4be1c3c7c5f4af
Mark <marknzl@outlook.co.nz>
2022-06-08 19:19:43 +1200

    Add error handling to webapp

--- app.py
+++ app.py
@@ -30,22 +30,32 @@ def index():
 
     if request.method == 'POST':
         file = request.files['file']
-        if file.filename.split(".")[1] != "png":
-            error_msg = "Only PNGs allowed!"
+        if file.filename.split('.')[1] != 'png':
+            error_msg = 'Only PNGs allowed!'
         else:
+            """
+            I compute the MD5 hash of the image here to use as the
+            unique file name. Also, I can use this to check if the same image
+            is uploaded multiple times which allows me to mitigate
+            expensive image processing operations.
+            """
+
             md5_hash = md5(file.read()).hexdigest()
             hashed_filename = f'{md5_hash}.{file.filename.split(".")[1]}'
             file.seek(0)    # need to reset cursor position as we call file.read()
 
-            if path.exists(f'static/{hashed_filename}'):    # If same image is already uploaded, we don't process it again.
-                extracted_filename = f'{hashed_filename.split(".")[0]}_plate.{hashed_filename.split(".")[1]}'
-            else:
-                file.save(f'static/{hashed_filename}')
-                extracted_filename = get_license_plate(hashed_filename)
-            raw_plate_text, accuracy = read_license_plate_text(extracted_filename)
-            filtered_plate_text = filter_text(raw_plate_text)
-
-            submitted = True
+            try:
+                # If same image is already uploaded, we don't process it again.
+                if path.exists(f'static/{hashed_filename}'):
+                    extracted_filename = f'{hashed_filename.split(".")[0]}_plate.{hashed_filename.split(".")[1]}'
+                else:
+                    file.save(f'static/{hashed_filename}')
+                    extracted_filename = get_license_plate(hashed_filename)
+                raw_plate_text, accuracy = read_license_plate_text(extracted_filename)
+                filtered_plate_text = filter_text(raw_plate_text)
+                submitted = True
+            except:
+                error_msg = 'An error occurred'
     
     return render_template('index.html',
                            submitted=submitted,
